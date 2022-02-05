1674a804e7756fecc9df4681dad4d115ca127dc0
wlandau <will.landau@gmail.com>
2022-02-05 18:14:44 -0500

    Fix #1

--- tests/testthat/test-crew_worker_loop.R
+++ tests/testthat/test-crew_worker_loop.R
@@ -12,7 +12,8 @@ test_that("no work then worker timeout", {
 })
 
 test_that("one job that runs and shutdowns the worker", {
-  dir_root <- fs::dir_create(tempfile())
+  dir_root <- tempfile()
+  dir_create(dir_root)
   store <- class_store_local$new(dir_root = dir_root)
   fun <- function(x, store) {
     fun <- function() rlang::abort(message = "x", class = "crew_shutdown")
@@ -32,4 +33,39 @@ test_that("one job that runs and shutdowns the worker", {
   expect_false(store$exists_input("worker"))
   expect_true(store$exists_output("worker"))
   expect_equal(store$read_output("worker")$value, 1L)
+})
+
+test_that("crew_worker_loop_monad() without errors", {
+  out <- crew_worker_loop_monad(fun = identity, args = list(x = "x"))
+  expect_equal(out$value, "x")
+  expect_true(is.numeric(out$seconds) && length(out$seconds) == 1)
+  expect_null(out$error)
+  expect_null(out$warnings)
+  expect_null(out$traceback)
+})
+
+
+test_that("crew_worker_loop_monad() with errors", {
+  fun <- function() {
+    warning("warning1")
+    warning("warning2")
+    stop("error1")
+  }
+  suppressWarnings(out <- crew_worker_loop_monad(fun = fun, args = list()))
+  expect_null(out$value)
+  expect_true(is.numeric(out$seconds))
+  expect_equal(length(out$seconds), 1)
+  expect_equal(out$error, "error1")
+  expect_true(is.character(out$traceback) && length(out$traceback) > 0)
+  expect_equal(out$warnings, c("warning1", "warning2"))
+})
+
+test_that("crew_worker_loop_monad() with worker shutdown", {
+  fun <- function() {
+    crew_shutdown()
+  }
+  expect_error(
+    crew_worker_loop_monad(fun = fun, args = list()),
+    class = "crew_shutdown"
+  )
 })

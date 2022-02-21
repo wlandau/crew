d2e9f1755011c342a8a2473e68ea66292187c216
wlandau <will.landau@gmail.com>
2022-02-21 15:42:30 -0500

    Lint

--- tests/performance/test-crew_queue_sync.R
+++ tests/performance/test-crew_queue_sync.R
@@ -2,19 +2,24 @@ proffer::pprof({
 library(crew)
 x <- crew:::crew_queue_sync$new()
 x$add_workers(4)
-for (i in 1:1000) {
+for (index in 1:1000) {
   x$push(
     fun = function(x) x,
-    args = list(x = i),
-    task = as.character(i)
+    args = list(x = index),
+    task = as.character(index)
   )
   out <- x$pop()
   if (!is.null(out)) {
     print(out$result$result)
   }
 }
-for (i in 1:1000) {
-  while (nrow(x$get_tasks()) || nrow(x$get_results()) || any(!x$get_workers()$free)) {
+not_done <- function(x) {
+  nrow(x$get_tasks()) ||
+    nrow(x$get_results()) ||
+    any(!x$get_workers()$free)
+}
+for (index in 1:1000) {
+  while (not_done(x)) {
     out <- x$pop()
     if (!is.null(out)) {
       print(out$result$result)

87fab48bd9f14058fe10427e79cba65de8a7a387
wlandau <will.landau@gmail.com>
2022-02-21 15:10:16 -0500

    Test abstract store class

---
+++ tests/testthat/test-crew_store.R
@@ -0,0 +1,25 @@
+test_that("get_root()", {
+  tmp <- tempfile()
+  x <- crew_store$new(dir_root = tmp)
+  expect_equal(x$get_root(), tmp)
+})
+
+test_that("main input", {
+  x <- crew_store$new()
+  expect_false(x$exists_main_input("worker"))
+  x$write_main_input("worker", value = "x")
+  expect_true(x$exists_main_input("worker"))
+  expect_equal(x$read_main_input("worker"), "x")
+  x$delete_main_input("worker")
+  expect_false(x$exists_main_input("worker"))
+})
+
+test_that("main output", {
+  x <- crew_store$new()
+  expect_false(x$exists_main_output("worker"))
+  x$write_main_output("worker", value = "x")
+  expect_true(x$exists_main_output("worker"))
+  expect_equal(x$read_main_output("worker"), "x")
+  x$delete_main_output("worker")
+  expect_false(x$exists_main_output("worker"))
+})

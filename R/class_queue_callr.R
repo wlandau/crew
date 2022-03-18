a1dbbb5dfe514872cc090c96b5927f96c610cfb8
wlandau <will.landau@gmail.com>
2022-03-18 16:24:44 -0400

    Inherit from parent queue class

--- R/class_queue_callr.R
+++ R/class_queue_callr.R
@@ -3,102 +3,27 @@
 #  under the MIT license. See also the `crew` package `NOTICE` file.
 queue_callr <- R6::R6Class(
   classname = "queue_callr",
+  inherit = queue,
   portable = FALSE,
   cloneable = FALSE,
   private = list(
-    tasks = NULL,
-    results = NULL,
-    workers = NULL,
-    initialize_tasks = function() {
-      private$tasks <- tibble::tibble(
-        task = character(0),
-        fun = list(NULL),
-        args = list(NULL)
-      )
-    },
-    initialize_results = function() {
-      private$results <- tibble::tibble(
-        task = character(0),
-        result = list(NULL)
-      )
-    },
     initialize_workers = function(workers, start) {
-      handles <- if_any(
-        start,
-        replicate(
+      super$initialize_workers(workers)
+      if (start) {
+        private$workers$handle <- replicate(
           workers,
           callr::r_session$new(
             wait = TRUE,
             options = callr::r_session_options(extra = list(supervise = TRUE))
           )
-        ),
-        replicate(workers, NULL)
-      )
-      private$workers <- tibble::tibble(
-        worker = uuid::UUIDgenerate(n = workers),
-        handle = handles,
-        free = rep(TRUE, workers),
-        sent = rep(FALSE, workers),
-        up = map_lgl(handles, ~!is.null(.x) && .x$is_alive()),
-        done = rep(FALSE, workers),
-        task = rep(NA_character_, workers),
-        fun = replicate(workers, NULL),
-        args = replicate(workers, NULL)
-      )
-    },
-    add_task = function(task, fun, args) {
-      dup <- task %in% private$tasks$task || task %in% private$workers$task
-      crew_assert(!dup, paste("duplicate task name", task))
-      private$tasks <- tibble::add_row(
-        .data = private$tasks,
-        task = task,
-        fun = list(fun),
-        args = list(args)
-      )
-    },
-    add_result = function(task, result) {
-      private$results <- tibble::add_row(
-        .data = private$results,
-        task = task,
-        result = list(result)
-      )
-    },
-    send_tasks = function() {
-      while (nrow(private$tasks) && any(private$workers$free)) {
-        index <- min(which(private$workers$free))
-        for (field in colnames(private$tasks)) {
-          private$workers[[field]][index] <- private$tasks[[field]][1]
-        }
-        for (field in c("free", "sent", "done")) {
-          private$workers[[field]][index] <- FALSE
-        }
-        private$tasks <- private$tasks[-1, ]
-      }
-    },
-    send_workers = function() {
-      workers <- private$workers
-      which <- !workers$free & !workers$sent
-      workers <- workers[which, ]
-      for (worker in workers$worker) {
-        index <- which(private$workers$worker == worker)
-        private$send_worker(
-          worker = worker,
-          fun = private$workers$fun[[index]],
-          args = private$workers$args[[index]]
         )
       }
     },
-    send_worker = function(worker, fun, args) {
+    send_worker = function(worker, fun, args, shutdown = FALSE) {
       index <- which(private$workers$worker == worker)
       handle <- private$workers$handle[[index]]
       handle$call(func = fun, args = args)
       private$workers$sent[index] <- TRUE
-    },
-    update_up = function() {
-      private$workers$up <- map_lgl(
-        private$workers$handle,
-        ~!is.null(.x) && .x$is_alive()
-      )
     },
     update_done = function() {
       index <- which(private$workers$sent)
@@ -106,14 +31,6 @@ queue_callr <- R6::R6Class(
       connections <- map(handles, ~.x$get_poll_connection())
       poll <- as.character(processx::poll(processes = connections, ms = 0))
       private$workers$done[index] <- poll == "ready"
-    },
-    update_crashed = function() {
-      x <- private$workers
-      crashed <- x$sent & !x$done & !x$up
-      if (any(crashed)) {
-        self$shutdown()
-        crew_error("worker crashed. Shutting down all workers.")
-      }
     },
     update_results = function() {
       for (index in seq_len(nrow(private$workers))) {
@@ -128,14 +45,6 @@ queue_callr <- R6::R6Class(
           private$workers$fun[index] <- list(NULL)
         }
       }
-    },
-    update = function() {
-      private$update_up()
-      private$update_done()
-      private$update_crashed()
-      private$update_results()
-      private$send_tasks()
-      private$send_workers()
     }
   ),
   public = list(
@@ -145,41 +54,7 @@ queue_callr <- R6::R6Class(
       private$initialize_workers(workers = workers, start = start)
       invisible()
     },
-    get_tasks = function() {
-      private$tasks
-    },
-    get_results = function() {
-      private$results
-    },
-    get_workers = function() {
-      private$workers
-    },
-    push = function(
-      fun,
-      args = list(),
-      task = uuid::UUIDgenerate(),
-      update = TRUE
-    ) {
-      fun <- rlang::as_function(fun)
-      private$add_task(fun = fun, args = args, task = task)
-      if (update) {
-        private$update()
-      }
-      invisible()
-    },
-    pop = function(update = TRUE) {
-      if (update) {
-        private$update()
-      }
-      results <- private$results
-      out <- NULL
-      if (nrow(results)) {
-        out <- list(task = results$task[1], result = results$result[[1]])
-        private$results <- private$results[-1, ]
-      }
-      out
-    },
-    shutdown = function() {
+    shutdown = function(wait = FALSE) {
       for (handle in private$workers$handle) {
         if (!is.null(handle)) {
           handle$kill()

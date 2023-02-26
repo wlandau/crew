bf14dc6bf3fbb6367554675f5714df41a967d04e
wlandau <will.landau@gmail.com>
2023-02-26 11:14:36 -0500

    Add socket utils

--- R/utils_network.R
+++ R/utils_network.R
@@ -1,3 +1,11 @@
+tcp_socket <- function(host, port) {
+  sprintf("tcp://%s:%s", host, as.character(as.integer(port)))
+}
+
+local_ipv4 <- function() {
+  getip::getip(type = "local")
+}
+
 random_port <- function(lower = 49152L, upper = 65535L) {
   ports <- seq.int(from = lower, to = upper, by = 1L)
   port <- parallelly::freePort(ports = ports)
--- tests/testthat/test-utils_network.R
+++ tests/testthat/test-utils_network.R
@@ -1,3 +1,19 @@
+crew_test("tcp_socket()", {
+  out <- tcp_socket(host = "127.0.0.1", port = 8000)
+  expect_equal(out, "tcp://127.0.0.1:8000")
+})
+
+crew_test("local_ipv4()", {
+  out <- local_ipv4()
+  expect_true(is.character(out))
+  expect_true(all(nzchar(out)))
+  expect_equal(length(out), 1L)
+  split <- strsplit(out, split = ".", fixed = TRUE)[[1]]
+  expect_equal(paste(split, collapse = "."), out)
+  expect_true(all(nzchar(split)))
+  expect_equal(length(split), 4L)
+})
+
 crew_test("random_port()", {
   port <- random_port(lower = 49152L, upper = 65355L)
   expect_true(is.character(port))

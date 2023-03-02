2c826a937c5d354e1ac789a15c9a51a47cf621ac
wlandau <will.landau@gmail.com>
2023-03-02 09:15:34 -0500

    available_sockets(), plural

--- tests/testthat/test-crew_mirai_router.R
+++ tests/testthat/test-crew_mirai_router.R
@@ -16,7 +16,7 @@ test_that("crew_mirai_router() works", {
   socket <- router$sockets()
   exp <- tcp_sockets(host = router$host, ports = router$ports)
   expect_equal(socket, exp)
-  expect_equal(router$available_socket(), socket)
+  expect_equal(router$available_sockets(), socket)
   px <- callr::r_session$new(wait = TRUE)
   px$call(function(socket) mirai::server(socket), args = list(socket = socket))
   for (index in seq_len(300)) {
@@ -26,7 +26,7 @@ test_that("crew_mirai_router() works", {
     Sys.sleep(0.1)
   }
   expect_equal(router$connections(), socket)
-  expect_equal(router$available_socket(), character(0))
+  expect_equal(router$available_sockets(), character(0))
   m <- mirai::mirai(ps::ps_pid())
   for (index in seq_len(300)) {
     if (!anyNA(m$data)) {

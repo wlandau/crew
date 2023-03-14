cd0029c2f6df429754f43b9d92233631eceeac98
wlandau <will.landau@gmail.com>
2023-03-13 20:12:05 -0400

    test controller utils

---
+++ tests/testthat/test-crew_controller.R
@@ -0,0 +1,36 @@
+crew_test("controller_demand()", {
+  expect_equal(controller_demand(tasks = 2L, workers = 4L), 0L)
+  expect_equal(controller_demand(tasks = 3L, workers = 4L), 0L)
+  expect_equal(controller_demand(tasks = 4L, workers = 4L), 0L)
+  expect_equal(controller_demand(tasks = 5L, workers = 4L), 1L)
+  expect_equal(controller_demand(tasks = 6L, workers = 5L), 1L)
+  expect_equal(controller_demand(tasks = 6L, workers = 4L), 2L)
+  expect_equal(controller_demand(tasks = 6L, workers = 3L), 3L)
+})
+
+crew_test("controller_n_new_workers()", {
+  expect_equal(
+    controller_n_new_workers(demand = 3L, auto_scale = "demand"),
+    3L
+  )
+  expect_equal(
+    controller_n_new_workers(demand = 0L, auto_scale = "demand"),
+    0L
+  )
+  expect_equal(
+    controller_n_new_workers(demand = 3L, auto_scale = "single"),
+    1L
+  )
+  expect_equal(
+    controller_n_new_workers(demand = 0L, auto_scale = "demand"),
+    0L
+  )
+  expect_equal(
+    controller_n_new_workers(demand = 3L, auto_scale = "none"),
+    0L
+  )
+  expect_equal(
+    controller_n_new_workers(demand = 0L, auto_scale = "none"),
+    0L
+  )
+})

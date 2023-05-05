231d4020050136b68ce0a5e2994107f2c23721db
wlandau <will.landau@gmail.com>
2023-05-05 10:17:41 -0400

    Increase seconds_exit

--- R/crew_launcher.R
+++ R/crew_launcher.R
@@ -73,7 +73,7 @@ crew_launcher <- function(
   seconds_timeout = 10,
   seconds_idle = Inf,
   seconds_wall = Inf,
-  seconds_exit = 0.1,
+  seconds_exit = 1,
   tasks_max = Inf,
   tasks_timers = 0L,
   reset_globals = TRUE,

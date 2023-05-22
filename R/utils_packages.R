3d4420078a985970aae9558ff7e4f75eec65a832
wlandau <will.landau@gmail.com>
2023-05-22 08:13:17 -0400

    recommend mirai and nanonext versions

--- R/utils_packages.R
+++ R/utils_packages.R
@@ -1,3 +1,38 @@
+# TODO: remove .onLoad() and dependent functions when
+# the next {nanonext} and {mirai} arrive on CRAN.
+# At that point, update the required versions of those packages.
+# nocov start
+.onLoad <- function(libname, pkgname) {
+  package_version_check(
+    package = "nanonext",
+    version = "0.8.3.9010",
+    repo = "https://shikokuchuo.r-universe.dev"
+  )
+  package_version_check(
+    package = "mirai",
+    version = "0.8.7.9013",
+    repo = "https://shikokuchuo.r-universe.dev"
+  )
+}
+# nocov end
+
+package_version_check <- function(package, version, repo) {
+  if (!rlang::is_installed(pkg = package, version = version)) {
+    package_version_message(package = package, version = version, repo = repo)
+  }
+}
+
+package_version_message <- function(package, version, repo) {
+  template <- paste(
+    "For {crew}, version %s or higher of packge {%s} is recommended.",
+    "If the current CRAN version of {%s} is too low, you can",
+    "install the development version using",
+    "install.packages(\"%s\", repos = \"%s\")"
+  )
+  message <- sprintf(template, version, package, package, package, repo)
+  rlang::inform(message = message)
+}
+
 load_packages <- function(packages, library) {
   out <- suppressPackageStartupMessages(
     lapply(

39cf488cac13e9527459060405d39f098d52b72d
wlandau-lilly <will.landau@lilly.com>
2023-06-09 12:10:45 -0400

    add a test

--- tests/testthat/test-crew_schedule.R
+++ tests/testthat/test-crew_schedule.R
@@ -1,9 +1,9 @@
-test_that("crew_schedule() validate empty", {
+test_that("schedule validate empty", {
   x <- crew_schedule()
   expect_silent(x$validate())
 })
 
-test_that("crew_schedule() validate full", {
+test_that("schedule validate full", {
   x <- crew_schedule()
   x$start()
   x$head <- "id"
@@ -11,7 +11,7 @@ test_that("crew_schedule() validate full", {
   expect_silent(x$validate())
 })
 
-test_that("crew_schedule() start", {
+test_that("schedule start", {
   x <- crew_schedule()
   expect_null(x$pushed)
   expect_null(x$collected)
@@ -20,13 +20,29 @@ test_that("crew_schedule() start", {
   expect_true(is.environment(x$collected))
 })
 
-test_that("crew_schedule() push", {
+test_that("schedule push", {
   x <- crew_schedule()
   x$start()
   expect_equal(length(x$pushed), 0L)
   expect_equal(length(x$collected), 0L)
+  expect_null(x$head)
+  expect_null(x$until)
   x$push(task = crew_null, id = "1")
   expect_equal(length(x$pushed), 1L)
   expect_equal(length(x$collected), 0L)
+  expect_null(x$head)
+  expect_null(x$until)
   expect_true(is_crew_null(x$pushed[["1"]]))
+})
+
+test_that("schedule collect without throttling", {
+  x <- crew_schedule()
+  x$start()
+  x$push(task = crew_null, id = "1")
+  x$collect(throttle = FALSE)
+  expect_equal(length(x$pushed), 0L)
+  expect_equal(length(x$collected), 1L)
+  expect_true(is_crew_null(x$collected[["1"]]))
+  expect_null(x$until)
+  expect_equal(x$head, "1")
 })

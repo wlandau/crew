9e0d5748a0e9a6d61c955ebfd354aa19f62d44a3
wlandau-lilly <will.landau@lilly.com>
2023-06-13 09:50:29 -0400

    clear out a test

--- tests/testthat/test-crew_controller.R
+++ tests/testthat/test-crew_controller.R
@@ -1,13 +1,3 @@
-crew_test("is_inactive()", {
-  daemons <- cbind(
-    online = c(0L, 1L, 0L, 0L, 1L, 0L),
-    instance = c(0L, 1L, 1L, 0L, 1L, 1L)
-  )
-  launching <- rep(c(TRUE, FALSE), each = 3L)
-  out <- is_inactive(daemons = daemons, launching = launching)
-  expect_equal(out, c(FALSE, FALSE, TRUE, TRUE, FALSE, TRUE))
-})
-
 crew_test("deprecate auto_scale", {
   skip_on_cran()
   expect_warning(

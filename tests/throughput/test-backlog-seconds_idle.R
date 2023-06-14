9d78d6c440f55297406cb2056fe04ae81da38a2c
wlandau <will.landau@gmail.com>
2023-06-14 07:31:21 -0400

    Add to tests

--- tests/throughput/test-backlog-seconds_idle.R
+++ tests/throughput/test-backlog-seconds_idle.R
@@ -26,4 +26,8 @@ system.time(
 )
 testthat::expect_equal(length(controller$schedule$pushed), 0L)
 testthat::expect_equal(length(controller$schedule$collected), 0L)
+controller$launcher$terminate()
+controller$launcher$poll()
+testthat::expect_equal(sum(controller$launcher$workers$assigned), n_tasks)
+testthat::expect_equal(sum(controller$launcher$workers$complete), n_tasks)
 controller$terminate()

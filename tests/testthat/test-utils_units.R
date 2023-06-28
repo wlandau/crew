9b19faf6e72a6b2d03d701b1c95e4c4cb0de49c1
wlandau-lilly <will.landau@lilly.com>
2023-06-27 22:25:27 -0400

    restore coverage

--- tests/testthat/test-utils_units.R
+++ tests/testthat/test-utils_units.R
@@ -1,4 +1,5 @@
 crew_test("units_time()", {
+  expect_match(units_time(NA_real_), "")
   expect_match(units_time(12), "seconds")
   expect_match(units_time(120), "minutes")
   expect_match(units_time(3720), "hours")

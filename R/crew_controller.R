6c338c946b22eaef3f0bc6c3ebb80979c0c6d201
wlandau-lilly <will.landau@lilly.com>
2023-09-20 12:03:16 -0400

    tally first

--- R/crew_controller.R
+++ R/crew_controller.R
@@ -201,8 +201,8 @@ crew_class_controller <- R6::R6Class(
     #' @param controllers Not used. Included to ensure the signature is
     #'   compatible with the analogous method of controller groups.
     launch = function(n = 1L, controllers = NULL) {
-      walk(x = self$launcher$done(), f = self$launcher$rotate)
       self$launcher$tally()
+      walk(x = self$launcher$done(), f = self$launcher$rotate)
       walk(x = self$launcher$unlaunched(n = n), f = self$launcher$launch)
       invisible()
     },

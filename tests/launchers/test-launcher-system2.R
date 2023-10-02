60814bb0c50cee23a0951d163d1f62179828145c
wlandau-lilly <will.landau@lilly.com>
2023-10-02 15:05:35 -0400

    docs

--- tests/launchers/test-launcher-system2.R
+++ tests/launchers/test-launcher-system2.R
@@ -45,7 +45,6 @@ crew_controller_system2 <- function(
   )
   launcher <- system2_launcher_class$new(
     name = name,
-    seconds_interval = seconds_interval,
     seconds_launch = seconds_launch,
     seconds_idle = seconds_idle,
     seconds_wall = seconds_wall,

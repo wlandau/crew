6ccbdb67cae2f0d9701a1c3b6e62f0fa7eb50719
wlandau-lilly <will.landau@lilly.com>
2023-11-16 12:46:03 -0500

    test stage pop()

--- tests/testthat/test-crew_stage.R
+++ tests/testthat/test-crew_stage.R
@@ -44,3 +44,28 @@ crew_test("crew_stage() resolved()", {
   expect_equal(x$resolved(), 741L)
 })
 
+crew_test("crew_stage() pop()", {
+  for (unpopped in c(0L, 7L)) {
+    x <- crew_stage()
+    x$start()
+    cv <- nanonext::cv()
+    x$inherit(cv)
+    expect_equal(nanonext::cv_value(x$condition), 0L)
+    replicate(3L, nanonext::cv_signal(cv))
+    x$unpopped <- unpopped
+    for (index in seq_len(3L) - 1L) {
+      expect_equal(nanonext::cv_value(x$condition), 3L - index)
+      expect_equal(x$unpopped, unpopped)
+      expect_equal(x$popped, 0L + index)
+      x$pop()
+    }
+    if (unpopped < 1L) {
+      expect_crew_error(x$pop())
+    } else {
+      expect_silent(x$pop())
+      expect_equal(nanonext::cv_value(x$condition), 0L)
+      expect_equal(x$unpopped, unpopped - 1L)
+      expect_equal(x$popped, 4L)
+    }
+  }
+})

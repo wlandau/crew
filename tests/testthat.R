7cd0baa79e5761a27417d64e29b025433e2d154f
wlandau-lilly <will.landau@lilly.com>
2023-11-30 13:10:10 -0500

    testing

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,10 +5,10 @@ test_check("crew")
 
 # nolint start
 # pkgload::load_all()
+# dirs <- list.dirs("tests", recursive = FALSE)
+# dirs <- dirs[!grepl("testthat", dirs)]
 # for (dir in list.dirs("tests", recursive = FALSE)) {
 #   message(dir)
 #   testthat::test_dir(dir)
 # }
-# testthat::test_dir("tests/interactive")
-# testthat::test_dir("tests/launchers")
 # nolint end

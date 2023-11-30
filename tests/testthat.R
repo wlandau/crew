c96928d52e20e23f9701f4a7cbacf5eb091b2713
wlandau-lilly <will.landau@lilly.com>
2023-11-30 13:15:50 -0500

    organize test code

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,13 +2,3 @@ library(testthat)
 library(crew)
 
 test_check("crew")
-
-# nolint start
-# pkgload::load_all()
-# dirs <- list.dirs("tests", recursive = FALSE)
-# dirs <- dirs[!grepl("testthat", dirs)]
-# for (dir in list.dirs("tests", recursive = FALSE)) {
-#   message(dir)
-#   testthat::test_dir(dir)
-# }
-# nolint end

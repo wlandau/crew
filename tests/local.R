ad36345da7409cb5a50f6247f57273c0ea658068
wlandau-lilly <will.landau@lilly.com>
2023-12-01 12:15:24 -0500

    Edit local test script

--- tests/local.R
+++ tests/local.R
@@ -1,10 +1,5 @@
 if (FALSE) {
   eval(parse(text = "pkgload::load_all()"))
   root <- eval(parse(text = "rprojroot::find_package_root_file()"))
-  dirs <- list.dirs(file.path(root, "tests"), recursive = FALSE)
-  dirs <- dirs[!grepl("testthat", dirs)]
-  for (dir in dirs) {
-    message(dir)
-    testthat::test_dir(dir)
-  }
+  testthat::test_dir(file.path(root, "tests", "local"))
 }

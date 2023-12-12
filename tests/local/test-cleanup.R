0449197c05681554e927c6a76f7660cf9744888d
wlandau-lilly <will.landau@lilly.com>
2023-12-12 08:53:12 -0500

    Add deliberately failing test of #141

---
+++ tests/local/test-cleanup.R
@@ -0,0 +1,19 @@
+crew_test("forcibly shut down workers on dispatcher disconnect", {
+  x <- crew_controller_local(workers = 1L)
+  x$start()
+  x$push(Sys.sleep(600))
+  Sys.sleep(2)
+  x$scale()
+  Sys.sleep(2)
+  dispatcher <- ps::ps_handle(pid = x$client$dispatcher)
+  ps::ps_kill(dispatcher)
+  crew_retry(
+    ~!ps::ps_is_running(dispatcher),
+    seconds_interval = 0.5,
+    seconds_timeout = 60
+  )
+  Sys.sleep(5)
+  worker <- x$launcher$workers$handle[[1L]]
+  on.exit(worker$kill())
+  expect_false(worker$is_alive())
+})

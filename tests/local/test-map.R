aea4b78d514b2462384933c905a7d9dd57dc2446
wlandau-lilly <will.landau@lilly.com>
2023-12-13 14:52:45 -0500

    Add a test

--- tests/local/test-map.R
+++ tests/local/test-map.R
@@ -30,3 +30,34 @@ crew_test("heavy task load for map()", {
   }
   controller$terminate()
 })
+
+crew_test("map() auto-scales", {
+  controller <- crew_controller_local(workers = 20L, tasks_max = 1L)
+  controller$start()
+  out <- controller$map(
+    command = {
+      if (x <= 100L) {
+        stop(x)
+      } else if (x <= 400L) {
+        warning(x)
+      }
+      x
+    },
+    iterate = list(x = seq_len(600L)),
+    error = "silent",
+    warnings = FALSE
+  )
+  testthat::expect_equal(length(controller$list), 0L)
+  sum <- controller$summary()
+  testthat::expect_equal(sum(sum$errors), 100L)
+  testthat::expect_equal(sum(sum$warnings), 300L)
+  name <- as.integer(out$name)
+  testthat::expect_equal(name, seq_len(600L))
+  testthat::expect_true(all((name <= 100L) == !is.na(out$error)))
+  testthat::expect_true(
+    all(
+      ((name > 100L) & (name <= 400L)) == !is.na(out$warnings)
+    )
+  )
+  controller$terminate()
+})

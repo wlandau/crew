260c0cfb450c8f0e3a7ad26615af7cf6f769cd43
wlandau-lilly <will.landau@lilly.com>
2024-01-04 12:43:45 -0500

    Try to remove detritus from tempdir()

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,9 @@
 library(testthat)
 library(crew)
 
+temp_old <- list.files(tempdir())
 test_check("crew")
+if (!interactive() && !isTRUE(as.logical(Sys.getenv("NOT_CRAN", "false")))) {
+  temp_new <- list.files(tempdir())
+  unlink(setdiff(x = temp_new, y = temp_old), recursive = TRUE)
+}

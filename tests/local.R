3f1d70c6e9492995289cc073830b305fbee333a1
wlandau <will.landau.oss@gmail.com>
2024-02-06 09:45:24 -0500

    Just remove tests/local.R

--- tests/local.R
+++
@@ -1,5 +0,0 @@
-if (FALSE) {
-  eval(parse(text = "pkgload::load_all()"))
-  root <- eval(parse(text = "rprojroot::find_package_root_file()"))
-  testthat::test_dir(file.path(root, "tests", "local"))
-}

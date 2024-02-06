e288a2628021dab9e5eaed65173f365331863f03
wlandau <will.landau.oss@gmail.com>
2024-02-06 08:58:00 -0500

    Stronger suppression of tests/local.R

--- tests/local.R
+++ tests/local.R
@@ -1,4 +1,4 @@
-if (!identical(Sys.getenv("CREW_TESTS", unset = ""), "")) {
+if (FALSE) {
   eval(parse(text = "pkgload::load_all()"))
   root <- eval(parse(text = "rprojroot::find_package_root_file()"))
   testthat::test_dir(file.path(root, "tests", "local"))

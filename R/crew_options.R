db73f98a3951b42d2865492c8a5a1b58921707eb
wlandau <will.landau.oss@gmail.com>
2024-10-08 08:27:26 -0400

    crew_options_local()

---
+++ R/crew_options.R
@@ -0,0 +1,9 @@
+#' @title Validate options.
+#' @export
+#' @family utilities
+#' @description Validate a `crew` options list.
+#' @return `NULL` (invisibly) on success, throws an error if there is
+#'   something wrong with the options list.
+crew_options_validate <- function(options) {
+  UseMethod("crew_options_validate")
+}
---
+++ R/crew_options_local.R
@@ -0,0 +1,45 @@
+#' @title Local crew controller options.
+#' @param log_directory Either `NULL` or a character of length 1
+#'   with the file path to a directory to write worker-specific log files
+#'   with standard output and standard error messages.
+#'   Each log file represents a single *instance* of a running worker,
+#'   so there will be more log files
+#'   if a given worker starts and terminates a lot. Set to `NULL` to suppress
+#'   log files (default).
+#' @param log_join Logical of length 1. If `TRUE`, `crew` will write
+#'   standard output and standard error to the same log file for
+#'   each worker instance. If `FALSE`, then they these two streams
+#'   will go to different log files with informative suffixes.
+#' @examples
+#'   crew_options_local()
+crew_options_local = function(
+  log_directory = NULL,
+  log_join = TRUE
+) {
+  out <- structure(
+    list(
+      log_directory = log_directory,
+      log_join = log_join
+    ),
+    class = c("crew_options_local", "crew_options")
+  )
+  crew_options_validate(out)
+  out
+}
+
+#' @export
+crew_options_validate.crew_options_local <- function(options) {
+  crew_assert(
+    options$log_directory %|||% "x",
+    is.character(.),
+    length(.) == 1L,
+    !anyNA(.),
+    nzchar(.),
+    message = "log_directory must be NULL or a valid directory path."
+  )
+  crew_assert(
+    options$log_join,
+    isTRUE(.) || isFALSE(.),
+    message = "log_join must be TRUE or FALSE."
+  )
+}

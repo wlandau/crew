888455e44dec5dd62205e83847490f67d923064b
wlandau <will.landau.oss@gmail.com>
2025-08-20 16:31:10 -0400

    collections 0.3.9

--- R/crew_package.R
+++ R/crew_package.R
@@ -19,6 +19,7 @@
 #'   [`clustermq`](https://mschubert.github.io/clustermq/),
 #'   and [`batchtools`](https://github.com/mlr-org/batchtools).
 #' @importFrom cli cli_progress_bar cli_progress_done cli_progress_update
+#' @importFrom collections queue
 #' @importFrom data.table rbindlist
 #' @importFrom later create_loop current_loop destroy_loop exists_loop later
 #' @importFrom mirai call_mirai daemon daemons is_mirai is_mirai_error
